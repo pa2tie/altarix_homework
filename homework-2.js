4032d699ba733cc2f592be5330f4ea97adc97116
Bobby <38836929+pa2tie@users.noreply.github.com>
2018-10-25 13:17:05 +0400

    Update homework-2.js
    working bind and getTotalSum functions

--- homework-2.js
+++ homework-2.js
@@ -1,20 +1,40 @@
 // принимает функцию и контекст вызова, возвращает функцию, 
 // которая всегда будет иметь корректный контекст вызова 
 
-function bind(func, context) { ... return function () { ... } } 
+function bind(func, context) { 
+	return function () { 
+  	func.apply(context, arguments);
+  } 
+} 
+
+function func() {
+	console.log(this);
+}
+
+var g = bind(func, "THIS");
+g(); // "THIS"
 
 
 // не принимает аргументов, возвращает функцию, 
 // которая возвращает результат parseInt, если вызвана с таким аргументом впервые, 
 // иначе не вычисляет заново, а возвращает сохранённое значение 
 
-function parseIntWithCashe() { ... return function () { ... } } 
+function parseIntWithCache() { 
+	var cache;
+	return function () {
+  	
+  } 
+} 
 
 
 // принимает любое количество аргументов, 
 // возвращает их сумму, без for
 
-function getTotalSum(a, b, c, ...) { ... } 
+function getTotalSum() { 
+  return [].reduce.call(arguments, (acc, el) => acc += el);
+} 
+
+console.log(getTotalSum(1, 2, 3)); // 6
 
 
 // * универсальный кэширующий декоратор
